6f4682f6df2f1d014bacafed884184f48f464248
Gbemiga Salam <holuwagbemiga97@gmail.com>
2024-02-23 14:35:14 +0100

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, SafeAreaView } from 'react-native';
+import tw from 'twrnc'
+import { LinearGradient } from 'expo-linear-gradient'
+import {Bars3CenterLeftIcon, BellIcon} from 'react-native-heroicons/solid'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+    colors={['rgba(58, 131, 244,0.4)', 'rgba(9, 181, 211, 0.4)']}
+    style={tw`w-full flex-1`}
+    >
+      <SafeAreaView>
+        <View style={tw`container`}>
+        
+        </View>
+      </SafeAreaView>
+      <StatusBar style='light'/>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
